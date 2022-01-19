283b53058f438c34be742572fdc9e6cbb6dba85a
Brian <brianbell212@gmail.com>
2022-01-19 11:21:03 -0500

    Create spotify API logic

---
+++ src/spotify.js
@@ -0,0 +1,22 @@
+// Used so that our spotify API logic stays organized
+
+// URL where we need to authenticate using Spotify.
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+// Gave in the Spotify Web API settings, this states where to take back the user if the Spotify login was successful.
+const redirectUri = "https://localhost:3000/";
+// Provided by spotify for verification
+const clientId = "6651b23db4ef414ab0e05fea9fa100a2";
+
+// Permissions which you need to ask Spotify for
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+// Called to authorize a user. Contains Client ID and all permissions so Spotify knows
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
