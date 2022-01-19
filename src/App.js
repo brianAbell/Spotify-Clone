f07b96e555f49dab9325539640ea868c3c024ee3
Brian <brianbell212@gmail.com>
2022-01-19 11:10:12 -0500

    Basic login screen added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-//import logo from './logo.svg';
 import React from "react";
 import './App.css';
+import Login from "./Login";
 
 function App() {
   return (
-    <div className="App">React App</div>
+    <div className="App">
+      <Login />
+      </div>
   );
 }
 
