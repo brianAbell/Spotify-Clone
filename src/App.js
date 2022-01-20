c628545cc3e41812cc78a6f7fe350cbd48aeda77
Brian <brianbell212@gmail.com>
2022-01-20 12:49:48 -0500

    General update

--- src/App.js
+++ src/App.js
@@ -1,69 +1,18 @@
-import React, { useEffect } from "react";
-import SpotifyWebApi from "spotify-web-api-js";
-import { useStateValue } from "./StateProvider";
-import Player from "./Player";
-import { getTokenFromResponse } from "./spotify";
+import logo from "./logo.svg";
 import "./App.css";
-import Login from "./Login";
-
-const s = new SpotifyWebApi();
+import { LeftMenu } from "./Components/LeftMenu";
+import { MainContainer } from "./Components/MainContainer";
+import { RightMenu } from "./Components/RightMenu";
 
 function App() {
-  const [{ token }, dispatch] = useStateValue();
-
-  useEffect(() => {
-    // Set token
-    const hash = getTokenFromResponse();
-    window.location.hash = "";
-    let _token = hash.access_token;
-
-    if (_token) {
-      s.setAccessToken(_token);
-
-      dispatch({
-        type: "SET_TOKEN",
-        token: _token,
-      });
-
-      s.getPlaylist("37i9dQZEVXcJZyENOWUFo7").then((response) =>
-        dispatch({
-          type: "SET_DISCOVER_WEEKLY",
-          discover_weekly: response,
-        })
-      );
-
-      s.getMyTopArtists().then((response) =>
-        dispatch({
-          type: "SET_TOP_ARTISTS",
-          top_artists: response,
-        })
-      );
-
-      dispatch({
-        type: "SET_SPOTIFY",
-        spotify: s,
-      });
-
-      s.getMe().then((user) => {
-        dispatch({
-          type: "SET_USER",
-          user,
-        });
-      });
-
-      s.getUserPlaylists().then((playlists) => {
-        dispatch({
-          type: "SET_PLAYLISTS",
-          playlists,
-        });
-      });
-    }
-  }, [token, dispatch]);
-
   return (
-    <div className="app">
-      {!token && <Login />}
-      {token && <Player spotify={s} />}
+    <div className="App">
+      <div className=""></div>
+      <LeftMenu />
+      <MainContainer />
+      <RightMenu />
+
+      <div className="background"></div>
     </div>
   );
 }
---
+++ src/Components/Songs.js
@@ -0,0 +1,57 @@
+const Songs = [
+  {
+    id: 1,
+    favourite: false,
+    songName: "Knife Talk (with 21 Savage ft. Project Pat)",
+    artist: "Drake",
+    song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Ringtone%2Fbellaciao.mp3?alt=media&token=e1dd8233-3521-4574-8957-d51af9a5db5c",
+    imgSrc:
+      "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Images%2Fmoneyheist.jpg?alt=media&token=32baf3d7-0ab5-47d5-ba48-554792cea117",
+  },
+  {
+    id: 2,
+    favourite: false,
+    songName: "Way 2 Sexy (With Future & Young Thug)",
+    artist: "Drake",
+    song: "",
+    imgSrc:
+      "https://freeimage.host/i/l3jeu2",
+  },
+  {
+    id: 3,
+    favourite: false,
+    songName: "Fair Trade (with Travis Scott)",
+    artist: "Drake",
+    song: "",
+    imgSrc:
+      "",
+  },
+  {
+    id: 4,
+    favourite: false,
+    songName: "One Dance",
+    artist: "Drake",
+    song: "",
+    imgSrc:
+      "",
+  },
+  {
+    id: 5,
+    favourite: false,
+    songName: "Girls Want Girls (with Lil Baby)",
+    artist: "Drake",
+    song: "",
+    imgSrc:
+      "",
+  },
+  {
+    id: 6,
+    songName: "Pop Style",
+    artist: "Drake",
+    song: "",
+    imgSrc:
+      "",
+  },
+];
+
+export { Songs };
